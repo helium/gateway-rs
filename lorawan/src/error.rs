e7e4aa71ff376cf815f121c0897b179f65600e04
lthiery <thiery.louis@gmail.com>
2021-10-08 13:32:29 -0700

    check for appropriate lengths while reading PHYPayload

--- lorawan/src/error.rs
+++ lorawan/src/error.rs
@@ -4,6 +4,7 @@ use std::{error::Error, fmt, io};
 pub enum LoraWanError {
     InvalidPacketType(u8),
     InvalidFPortForFopts,
+    InvalidPacketSize(super::MType, usize),
     Io(io::Error),
 }
 
@@ -12,6 +13,9 @@ impl fmt::Display for LoraWanError {
         match self {
             LoraWanError::InvalidPacketType(v) => write!(f, "Invalid packet type: {:#02x}", v),
             LoraWanError::InvalidFPortForFopts => write!(f, "Invalid: fport 0 with fopts"),
+            LoraWanError::InvalidPacketSize(mtype, s) => {
+                write!(f, "Invalid packet size {} for type {:?}", s, mtype)
+            }
             LoraWanError::Io(err) => err.fmt(f),
         }
     }
