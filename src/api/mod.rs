0f90e5d7a906a4b27127672d997233c51d1220cd
Marc Nijdam <marc_nijdam@mac.com>
2022-01-03 17:12:00 -0700

    Add local ecdh api support

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -6,6 +6,7 @@ pub const CONNECT_URI: &str = "http://127.0.0.1:4467";
 
 pub use client::LocalClient;
 pub use helium_proto::services::local::{
-    ConfigReq, ConfigRes, ConfigValue, HeightReq, HeightRes, PubkeyReq, PubkeyRes, SignReq, SignRes,
+    ConfigReq, ConfigRes, ConfigValue, EcdhReq, EcdhRes, HeightReq, HeightRes, PubkeyReq,
+    PubkeyRes, SignReq, SignRes,
 };
 pub use server::LocalServer;
