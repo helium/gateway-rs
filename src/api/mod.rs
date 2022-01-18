6ea4e8745a2e1f54c0edd97dffc9de559676d8f9
jeffgrunewald <jgrunewald@helium.com>
2022-01-18 15:45:14 -0500

    using format macro instead of string manipulation

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,8 +1,7 @@
 mod client;
 mod server;
 
-const LISTEN_ADDR: &str = "127.0.0.1:";
-const URI_PREFIX: &str = "http://";
+const LISTEN_ADDR: &str = "127.0.0.1";
 
 pub use client::LocalClient;
 pub use helium_proto::services::local::{
@@ -12,14 +11,9 @@ pub use helium_proto::services::local::{
 pub use server::LocalServer;
 
 pub fn listen_addr(port: u16) -> String {
-    let mut address = LISTEN_ADDR.to_string();
-    address += &port.to_string();
-    address
+    format!("{}:{}", LISTEN_ADDR, port)
 }
 
 pub fn connect_uri(port: u16) -> String {
-    let mut uri = URI_PREFIX.to_string();
-    let addr = listen_addr(port);
-    uri += &addr;
-    uri
+    format!("http://{}", listen_addr(port))
 }
