09d91c3ddb042503399bea755f5488bb429567e0
Louis Thiery <thiery.louis@gmail.com>
2022-02-14 06:19:54 -0800

    update semtech-udp to 0.8 (#189)
    * update semtech-udp to 0.8
    
    * combine udp parse error log into a single line
    
    * display unparsable gwmp frames as warning

--- src/gateway.rs
+++ src/gateway.rs
@@ -49,7 +49,7 @@ impl Gateway {
     ) -> Result<Self> {
         let gateway = Gateway {
             uplinks,
-            downlink_mac: MacAddress::new(&[0u8; 8]),
+            downlink_mac: [0u8; 8].into(),
             messages,
             udp_runtime: UdpRuntime::new(&settings.listen).await?,
         };
@@ -80,8 +80,11 @@ impl Gateway {
 
     async fn handle_udp_event(&mut self, logger: &Logger, event: Event) -> Result {
         match event {
-            Event::UnableToParseUdpFrame(buf) => {
-                info!(logger, "ignoring semtech udp parsing error for {:?}", buf)
+            Event::UnableToParseUdpFrame(e, buf) => {
+                warn!(
+                    logger,
+                    "ignoring semtech udp parsing error {e}, raw bytes {buf:?}"
+                );
             }
             Event::NewClient((mac, addr)) => {
                 info!(logger, "new packet forwarder client: {}, {}", mac, addr);
