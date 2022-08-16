603e7c6b469a13571ce0d3119e8bb86641e6f3ec
Jay Kickliter <jay@kickliter.com>
2022-08-16 10:31:22 -0600

    Pull in off-chain PoC protos

--- src/cmd/info.rs
+++ src/cmd/info.rs
@@ -177,9 +177,9 @@ impl InfoCache {
             .and_then(KeyedUri::try_from)
     }
 
-    async fn gateway_version(&mut self) -> Result<GatewayVersion> {
+    async fn gateway_version(&mut self) -> Result<Option<GatewayVersion>> {
         let height = self._height().await?;
-        Ok(GatewayVersion::from(height.gateway_version))
+        Ok(height.gateway_version.map(GatewayVersion::from))
     }
 
     async fn region(&mut self) -> Result<Region> {
