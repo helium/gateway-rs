a5db8461edfa01adbdee2d371f0e8cbb0b5211c8
Jay Kickliter <jay@kickliter.com>
2022-08-23 16:15:02 -0600

    Fix channel

--- src/service/poc.rs
+++ src/service/poc.rs
@@ -7,35 +7,54 @@ use helium_proto::services::{
     Channel, Endpoint,
 };
 use http::Uri;
+use tonic::{
+    metadata::{Ascii, MetadataValue},
+    service::{interceptor::InterceptedService, Interceptor},
+};
 
-type PocLoraClient = Client<Channel>;
+type PocLoraClient = Client<InterceptedService<Channel, PocLorReqInterceptor>>;
 
 #[derive(Debug)]
-pub struct PocLoraService {
-    client: PocLoraClient,
-}
+pub struct PocLoraService(PocLoraClient);
 
 impl PocLoraService {
-    // TODO: Use keyed URI
     pub fn new(uri: Uri) -> Self {
         let channel = Endpoint::from(uri)
             .connect_timeout(CONNECT_TIMEOUT)
             .timeout(RPC_TIMEOUT)
             .connect_lazy();
-        Self {
-            client: PocLoraClient::new(channel),
-        }
+        let metadata_val = std::env::var("POC_LORA_AUTH_TOKEN")
+            .ok()
+            .map(|token| MetadataValue::try_from(format!("Bearer {}", token)).unwrap());
+        let client: PocLoraClient =
+            Client::with_interceptor(channel, PocLorReqInterceptor(metadata_val));
+        Self(client)
     }
 
     pub async fn submit_beacon(&mut self, report: LoraBeaconReportReqV1) -> Result<String> {
-        let resp = self.client.submit_lora_beacon(report).await?;
-        // TODO: verify with pubkey
+        let resp = self.0.submit_lora_beacon(report).await?;
         Ok(resp.into_inner().id)
     }
 
     pub async fn submit_witness(&mut self, report: LoraWitnessReportReqV1) -> Result<String> {
-        let resp = self.client.submit_lora_witness(report).await?;
-        // TODO: verify with pubkey
+        let resp = self.0.submit_lora_witness(report).await?;
         Ok(resp.into_inner().id)
+    }
+}
+
+// We need to intercept requests in order to add the auth-token.
+struct PocLorReqInterceptor(Option<MetadataValue<Ascii>>);
+
+impl Interceptor for PocLorReqInterceptor {
+    fn call(
+        &mut self,
+        mut request: tonic::Request<()>,
+    ) -> std::result::Result<tonic::Request<()>, tonic::Status> {
+        if let Some(metadata_val) = &self.0 {
+            request
+                .metadata_mut()
+                .insert("authorization", metadata_val.clone());
+        };
+        Ok(request)
     }
 }
