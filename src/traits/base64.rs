aec537563e623b166ffd81ecba49406b0d269e23
Marc Nijdam <marc_nijdam@mac.com>
2022-12-16 07:25:16 -0500

    bump deps, fix base64 changes

--- src/traits/base64.rs
+++ src/traits/base64.rs
@@ -1,16 +1,21 @@
+const URL_SAFE_ENGINE: base64::engine::fast_portable::FastPortable =
+    base64::engine::fast_portable::FastPortable::from(
+        &base64::alphabet::URL_SAFE,
+        base64::engine::fast_portable::NO_PAD,
+    );
 pub trait Base64 {
     fn to_b64url(&self) -> String
     where
         Self: AsRef<[u8]>,
     {
-        base64::encode_config(self.as_ref(), base64::URL_SAFE_NO_PAD)
+        base64::encode_engine(self.as_ref(), &URL_SAFE_ENGINE)
     }
 
     fn to_b64(&self) -> String
     where
         Self: AsRef<[u8]>,
     {
-        base64::encode_config(self.as_ref(), base64::STANDARD)
+        base64::encode(self.as_ref())
     }
 }
 
