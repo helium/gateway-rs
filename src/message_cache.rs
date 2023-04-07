7bb6e099fa8d60b147acf57cd4dd999b82e36e90
Marc Nijdam <marc_nijdam@mac.com>
2023-04-07 15:54:41 -0500

    Don’t reconnect immediately on router failure
    This change will recalculate the reconnect timer based on the fact that there was an error during the router.
    
    If an error happens during a fresh connection the reconnect timer will be set to a short time using `next_connect` which will not try to connect immediately. The subseequent timer firing will end up reconnecting the router and sending the messages

--- src/message_cache.rs
+++ src/message_cache.rs
@@ -9,19 +9,19 @@ pub struct MessageCache<T> {
     max_messages: u16,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct CacheMessage<T> {
     received: Instant,
     message: T,
 }
 
 impl<T> CacheMessage<T> {
-    pub fn hold_time(&self) -> Duration {
-        self.received.elapsed()
+    pub fn new(message: T, received: Instant) -> Self {
+        Self { message, received }
     }
 
-    pub fn into_inner(self) -> T {
-        self.message
+    pub fn hold_time(&self) -> Duration {
+        self.received.elapsed()
     }
 }
 
@@ -42,11 +42,29 @@ impl<T> MessageCache<T> {
         }
     }
 
+    /// Pushes a given at the end of the cache. The message is tagged with the
+    /// given received time which can be used to calculate hold time of a
+    /// packet.
+    ///
+    /// Pushing a packet onto the back of a full cache will cause the oldest
+    /// (first) message in the cache to be dropped.
     pub fn push_back(&mut self, message: T, received: Instant) {
-        self.waiting.push_back(CacheMessage { message, received });
+        self.waiting.push_back(CacheMessage::new(message, received));
         if self.len() > self.max_messages as usize {
             self.waiting.pop_front();
         }
+    }
+
+    /// Pushes a CacheMessage back on the front of the queue. This is useful to
+    /// push a packet back at the front after a failed delivery attempt.
+    ///
+    /// Pushing to the front of a full cache will cause the given message to not
+    /// be added.
+    pub fn push_front(&mut self, cache_message: CacheMessage<T>) {
+        if self.len() > self.max_messages as usize {
+            return;
+        }
+        self.waiting.push_front(cache_message);
     }
 
     pub fn pop_front(&mut self, duration: Duration) -> (usize, Option<CacheMessage<T>>) {
